2953ce75724952c730a19c072d8d866e92f075bb
nL1x <nathan.jeannot@epitech.eu>
2024-12-03 13:20:02 +0100

    feat(back/controllers/user/fetch): adds getMe & getUser

--- back/src/controllers/user/fetch.ts
+++ back/src/controllers/user/fetch.ts
@@ -1,7 +1,9 @@
 import User from "@models/user";
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import {CODE_STATUS} from "@config/variables";
-import handleRequestErrors from "@errors/sequelize";
+import handleRequestError from "@errors/sequelize";
+import CustomError, {CUSTOM_ERROR_TYPE} from "@errors/custom-error";
+import {AuthenticatedRequest} from "@utils/token";
 
 // ===============================
 //   TODO : Review this function
@@ -17,6 +19,60 @@ export async function getUsers(req: Request, res: Response)
             });
         })
         .catch((error) => {
-           handleRequestErrors(res, error);
+           handleRequestError(res, error);
         });
+}
+
+export async function getUser(req: Request, res: Response)
+{
+    const { id: userId } = req.params;
+
+    if (!userId)
+        return handleRequestError(res, new CustomError(
+            CUSTOM_ERROR_TYPE.MISSING_PARAMETER,
+            "The parameter 'id' is missing."
+        ));
+
+    // Check if the userId is a positive integer
+    if (!(/^\d+$/.test(userId)))
+        return handleRequestError(res, new CustomError(
+            CUSTOM_ERROR_TYPE.INCORRECT_PARAMETER,
+            "The parameter 'id' should be a positive integer."
+        ));
+
+    const user = await User.findByPk(parseInt(userId), {
+        attributes: {
+            exclude: ['password']
+        }
+    });
+
+    if (user === null) {
+        return handleRequestError(res, new CustomError(
+            CUSTOM_ERROR_TYPE.USER_NOT_FOUND,
+            "The requested user cannot be found."
+        ));
+    }
+
+    res.status(CODE_STATUS.SUCCESS).json({
+        "user": user
+    });
+}
+
+export async function getMe(req: Request, res: Response)
+{
+    const authReq = req as AuthenticatedRequest;
+    const { id: userId } = req.body;
+
+    const user = await User.findByPk(userId);
+
+    if (user === null) {
+        return handleRequestError(res, new CustomError(
+            CUSTOM_ERROR_TYPE.USER_NOT_FOUND,
+            "The requested user cannot be found."
+        ));
+    }
+
+    res.status(CODE_STATUS.SUCCESS).json({
+        "user": user
+    });
 }
