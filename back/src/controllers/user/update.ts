0b4bcd6574face7236799ce849db4315c98bedd0
nL1x <nathan.jeannot@epitech.eu>
2024-12-03 13:22:55 +0100

    feat(back/controllers/user/update): adds an update user route

--- back/src/controllers/user/update.ts
+++ back/src/controllers/user/update.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 import { AuthenticatedRequest } from "@utils/token";
+import {deleteAvatar, checkAvatar, saveAvatarFile} from "@utils/avatar";
+import CustomError from "@errors/custom-error";
+import handleRequestError from "@errors/sequelize";
+import {CODE_STATUS} from "@config/variables";
 
 export default async function updateUser(req: Request, res: Response)
 {
@@ -11,10 +15,23 @@ export default async function updateUser(req: Request, res: Response)
         authReq.user.password = password;
     if (username)
         authReq.user.username = username;
-    if (avatar)
-        // set avatar after checking its correct
-        return;
 
-    // TODO : Finish this part...
+    if (avatar) {
+        const avatarError = checkAvatar(avatar);
+        if (avatarError instanceof CustomError)
+            return handleRequestError(res, avatarError);
 
+        await saveAvatarFile(authReq.user, avatar.path);
+    }
+
+    // If the username unique constraint is invalid, then the error is caught and handled.
+    try {
+        await authReq.user.save();
+    } catch (error) {
+        return handleRequestError(res, error);
+    }
+
+    res.status(CODE_STATUS.SUCCESS).json({
+        "message": "Your profile has been successfully updated."
+    });
 }
