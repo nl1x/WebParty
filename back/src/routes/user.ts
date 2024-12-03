d37f72445191527bd6fedeb401bc3f810ba9b93f
nL1x <nathan.jeannot@epitech.eu>
2024-12-03 13:23:31 +0100

    feat(back/routes): adds get specific user route

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import updateUser from "@controllers/user/update";
-import { getUsers } from "@controllers/user/fetch";
+import {getUser, getUsers} from "@controllers/user/fetch";
 import { file } from "@middlewares/upload-picture";
 import checkAuthentication from "@middlewares/check-authentication";
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
+userRouter.get('/:id', getUser);
 userRouter.patch('/', checkAuthentication, file('avatar', 'uploads/avatars/'), updateUser);
 
 export default userRouter;
