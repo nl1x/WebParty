80a112b885e991c016d69008bdbf7dedf6c4de1f
nL1x <nathan.jeannot@epitech.eu>
2024-12-28 05:05:34 +0100

    feat(front/routes): adds login, logout, home & camera routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,9 @@ import {NavigatorProvider} from "@hooks/navigator/navigator-provider.tsx";
 import {AuthProvider} from "@hooks/auth/auth-provider.tsx";
 import ProtectedRoute from "@components/protected-route/protected-route.tsx";
 import HomeView from "@views/home/home-view.tsx";
+import LogoutView from "@views/logout/logout-view.tsx";
+import {PATH} from "@path/path.tsx";
+import CameraView from "@views/camera/camera-view.tsx";
 
 function App() {
 
@@ -15,11 +18,17 @@ function App() {
           <AuthProvider>
             <Routes>
               <Route path="/">
-                <Route index element={<h1>Vite + React index</h1>}/>
-                <Route path="login" element={<LoginView />}/>
-                <Route path="home" element={
-                  <ProtectedRoute redirect='/login'>
+                <Route index element={<LoginView />}/>
+                <Route path={PATH.LOGIN} element={<LoginView />}/>
+                <Route path={PATH.LOGOUT} element={<LogoutView />}/>
+                <Route path={PATH.HOME} element={
+                  <ProtectedRoute redirect={PATH.LOGIN}>
                     <HomeView />
+                  </ProtectedRoute>
+                }/>
+                <Route path={PATH.CAMERA} element={
+                  <ProtectedRoute redirect={PATH.LOGIN}>
+                    <CameraView />
                   </ProtectedRoute>
                 }/>
                 <Route path="*" element={<h1>Vite + React /*</h1>}/>
