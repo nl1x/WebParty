f5ff81f49cee309411576b21259b9319b768cd9e
nL1x <nathan.jeannot@epitech.eu>
2024-12-28 05:07:12 +0100

    refactor(front/hooks/auth-provider): move the profile into the auth provider and adds an updateUserProfile method to fetch the profile

--- front/src/hooks/auth/auth-provider.tsx
+++ front/src/hooks/auth/auth-provider.tsx
@@ -3,6 +3,9 @@ import { useMemo } from 'react';
 import { createContext } from 'react';
 import { useContext } from 'react';
 import Cookies from "js-cookie";
+import getMe from "@api/user.ts";
+import useNavigatorContext from "@hooks/navigator/navigator-provider.tsx";
+import {PATH} from "@path/path.tsx";
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -10,27 +13,81 @@ interface AuthProviderProps {
 
 interface AuthContextType {
   user: object|null;
-  setUser:  React.Dispatch<React.SetStateAction<null>>;
+  setUser: React.Dispatch<React.SetStateAction<null>>;
+  profile: UserProfileProps|null;
+  updateUserProfile: () => Promise<true | false>;
   isLoggedIn: () => boolean;
+}
+
+
+export interface RoleProps {
+    name: string;
+    displayName: string;
+    weight: number;
+}
+
+export interface ActionProps {
+    id: number;
+    proofPicture: string;
+    status: string;
+    action: {
+        description: string;
+        difficulty: number;
+        requireProof: boolean;
+    };
+}
+
+export interface UserProfileProps {
+    me: {
+        id: number;
+        username: string;
+        avatarUrl: string;
+        createdAt: string;
+        updatedAt: string;
+        role: RoleProps;
+        history: ActionProps[];
+        action?: ActionProps;
+    }
 }
 
 const AuthContext = createContext<AuthContextType|undefined>(undefined);
 
 export function AuthProvider(props: AuthProviderProps) {
   const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState<UserProfileProps|any|null>(null);
+  const {navigate} = useNavigatorContext();
+
   const isLoggedIn = useCallback(() => {
     const session = Cookies.get('session');
 
     return !(!session);
   }, []);
 
+  const updateUserProfile = useCallback(async () => {
+    try {
+      const response = await getMe();
+
+      if (response.status === 401) {
+        navigate(PATH.LOGIN);
+        return false;
+      }
+
+      setProfile(response.data);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }, []);
+
   const memoValue = useMemo(
     () => ({
       user,
       setUser,
-      isLoggedIn
+      profile,
+      updateUserProfile,
+      isLoggedIn,
     }),
-    [user, setUser, isLoggedIn]
+    [user, setUser, profile, updateUserProfile, isLoggedIn]
   );
 
   return <AuthContext.Provider value={memoValue}>{props.children}</AuthContext.Provider>;
