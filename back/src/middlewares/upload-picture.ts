6dea269d865c0dbfe28f95e24c7d9abeefad8010
nL1x <nathan.jeannot@epitech.eu>
2024-12-29 17:09:16 +0100

    feat(back/uploads): use a different directory depending on the app status (development or production)

--- back/src/middlewares/upload-picture.ts
+++ back/src/middlewares/upload-picture.ts
@@ -1,13 +1,29 @@
 import multer from "multer";
+import {DEFAULT} from "@config/variables";
+import fs from "fs";
+
+async function createDirectoryIfNotExists(dir: string)
+{
+    try {
+        await fs.promises.access(dir);
+    } catch (error) {
+        await fs.promises.mkdir(dir, { recursive: true });
+    }
+}
 
 export function file(fieldName: string, destination: string) {
 
     const storage = multer({
         storage: multer.diskStorage({
-            destination: function (req, file, cb) {
-                cb(null, destination);
+            destination: async function (req, file, cb) {
+                const finalDestination = DEFAULT.ENVIRONMENT === 'production'
+                    ? `${DEFAULT.UPLOAD_DIR}/${destination}`
+                    : destination;
+
+                await createDirectoryIfNotExists(finalDestination);
+                cb(null, finalDestination);
             },
-            filename: function (req, file, cb) {
+            filename: async function (req, file, cb) {
                 cb(null, `temp_${Date.now()}_${file.originalname}`);
             }
         })
