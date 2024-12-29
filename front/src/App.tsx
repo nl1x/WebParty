33d8018b503fa2b2aee318f07aa1bd0a1bf7f679
nL1x <nathan.jeannot@epitech.eu>
2024-12-29 05:19:21 +0100

    feat(front/path): adds admin, rank, history & account routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,20 @@
-import Background from "./components/background/background.tsx";
+import Background from "./components/background/background";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginView from "@views/login/login-view";
-import {NavigatorProvider} from "@hooks/navigator/navigator-provider.tsx";
-import {AuthProvider} from "@hooks/auth/auth-provider.tsx";
-import ProtectedRoute from "@components/protected-route/protected-route.tsx";
-import HomeView from "@views/home/home-view.tsx";
-import LogoutView from "@views/logout/logout-view.tsx";
-import {PATH} from "@path/path.tsx";
-import CameraView from "@views/camera/camera-view.tsx";
+import {NavigatorProvider} from "@hooks/navigator/navigator-provider";
+import {AuthProvider} from "@hooks/auth/auth-provider";
+import ProtectedRoute from "@components/protected-route/protected-route";
+import HomeView from "@views/home/home-view";
+import LogoutView from "@views/logout/logout-view";
+import {PATH} from "@path/path";
+import CameraView from "@views/camera/camera-view";
+import {ROLE} from "@config/variables.ts";
+import AdminView from "@views/admin/admin-view.tsx";
+import Redirect from "@components/redirect/redirect.tsx";
+import NotFoundView from "@views/not-found/not-found-view.tsx";
+import HistoryView from "@views/history/history-view.tsx";
+import AccountView from "@views/account/account-view.tsx";
+import RankView from "@views/rank/rank-view.tsx";
 
 function App() {
 
@@ -31,7 +38,28 @@ function App() {
                     <CameraView />
                   </ProtectedRoute>
                 }/>
-                <Route path="*" element={<h1>Vite + React /*</h1>}/>
+                <Route path={PATH.RANK} element={
+                  <ProtectedRoute redirect={PATH.LOGIN}>
+                    <RankView />
+                  </ProtectedRoute>
+                }/>
+                <Route path={PATH.HISTORY} element={
+                  <ProtectedRoute redirect={PATH.LOGIN}>
+                    <HistoryView />
+                  </ProtectedRoute>
+                }/>
+                <Route path={PATH.ACCOUNT} element={
+                  <ProtectedRoute redirect={PATH.LOGIN}>
+                    <AccountView />
+                  </ProtectedRoute>
+                }/>
+                <Route path={PATH.ADMIN} element={
+                  <ProtectedRoute redirect={PATH.NOT_FOUND} requiredRole={ROLE.ORGANISER}>
+                    <AdminView />
+                  </ProtectedRoute>
+                }/>
+                <Route path="*" element={<Redirect to={PATH.NOT_FOUND}/>}/>
+                <Route path={PATH.NOT_FOUND} element={<NotFoundView/>}/>
               </Route>
             </Routes>
           </AuthProvider>
